a0113eb57eb5fbe3a3f89221ff8a7d0a9f05b9d4
Alexander Harkness <bearbin@gmail.com>
2014-02-10 07:21:09 +0000

    Update gzcompress.py

--- gzcompress.py
+++ gzcompress.py
@@ -37,7 +37,4 @@ def is_updated(checkPath, againstPath):
 
 	Returns True or False
 	"""
-	if os.path.getmtime(checkPath) > os.path.getmtime(againstPath):
-		return True
-	else:
-		return False
+	return os.path.getmtime(checkPath) > os.path.getmtime(againstPath)
